29ad0580c4f06a60faf8a039bf42ede16713441b
sonali dsouza <sonali.18feb@gmail.com>
2020-02-06 23:42:12 -0800

    Add project files.

---
+++ UnitTests/Models/ItemModelTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class ItemModelTests
+    {
+        [Test]
+        public void ItemModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ItemModel();
+            
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ UnitTests/ViewModels/ItemIndexViewModelTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Game.ViewModels;
+using Xamarin.Forms.Mocks;
+using Xamarin.Forms;
+using Game.Services;
+using System.Threading.Tasks;
+
+namespace UnitTests.ViewModels
+{
+    public class ItemIndexViewModelTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            // Activate the Datastore
+            ScoreIndexViewModel.Instance.GetCurrentDataSource();
+            ItemIndexViewModel.Instance.GetCurrentDataSource();
+        }
+
+        [Test]
+        public async Task ItemIndexViewModel_Read_Invalid_ID_Bogus_Should_Fail()
+        {
+            // Arrange
+
+            // Act
+            var result = await ItemIndexViewModel.Instance.ReadAsync("bogus");
+
+            // Reset
+
+            // Assert
+            Assert.IsNull(result);
+        }
+    }
+}
