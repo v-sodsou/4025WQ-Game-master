cffc19f816f4abed3708c1c43ed84756a69a8e97
sonali dsouza <sonali.18feb@gmail.com>
2020-02-08 11:11:37 -0800

    Added CharacterCreatePage file to the Views

---
+++ Game/Game/Views/Characters/CharacterCreatePage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using Game.Models;
+using Game.ViewModels;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Create Character
+    /// </summary>
+    [DesignTimeVisible(false)]
+    public partial class CharacterCreatePage : ContentPage
+    {
+        // The character to create
+        GenericViewModel<Character> ViewModel { get; set; }
+
+        /// <summary>
+        /// Constructor for Create makes a new model
+        /// </summary>
+        public CharacterCreatePage(GenericViewModel<Character> data)
+        {
+            InitializeComponent();
+
+            data.Data = new Character();
+
+            BindingContext = this.ViewModel = data;
+
+            this.ViewModel.Title = "Create Character";
+
+        }
+
+  
+
+  
+      
+    }
+}
