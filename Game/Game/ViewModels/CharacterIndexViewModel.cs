5dc550bc6441db4c5c46e0898805e38b7d824e95
sonali dsouza <sonali.18feb@gmail.com>
2020-02-07 22:45:55 -0800

    Make CharacterIndexViewModel singleton and add a constructor

--- Game/Game/ViewModels/CharacterIndexViewModel.cs
+++ Game/Game/ViewModels/CharacterIndexViewModel.cs
@@ -1,9 +1,53 @@
 ﻿using Game.Models;
+using System;
 
 namespace Game.ViewModels
 {
     public class CharacterIndexViewModel : BaseViewModel<Character>
     {
-        
+        #region Singleton
+
+        // Make this a singleton so it only exist one time because holds all the data records in memory
+        private static volatile CharacterIndexViewModel instance;
+        private static readonly object syncRoot = new Object();
+
+        public static CharacterIndexViewModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new CharacterIndexViewModel();
+                            instance.Initialize();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion Singleton
+
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// 
+        /// The constructor subscribes message listeners for crudi operations
+        /// </summary>
+        public CharacterIndexViewModel()
+        {
+            Title = "Characters";
+        }
+
+        #endregion Constructor
     }
+
+
 }
