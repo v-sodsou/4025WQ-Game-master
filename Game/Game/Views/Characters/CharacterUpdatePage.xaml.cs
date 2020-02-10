14342277f136ba5f8efe66a53c9df97c10c19964
maencina <mencina@cityuniversity.edu>
2020-02-10 14:08:40 -0800

    Added Character Update page

---
+++ Game/Game/Views/Characters/CharacterUpdatePage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game.Models;
+using Game.ViewModels;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views.Characters
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CharacterUpdatePage : ContentPage
+    {
+        // View Model for Item
+        readonly GenericViewModel<Character> ViewModel;
+
+        public CharacterUpdatePage(GenericViewModel<Character> data)
+        {
+            InitializeComponent();
+
+            BindingContext = this.ViewModel = data;
+        }
+
+        async void Save_Clicked(object sender, EventArgs e)
+        {
+            MessagingCenter.Send(this, "Update", ViewModel.Data);
+
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Cancel and close this page
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            // Pop the modal Update page off the ModalStack
+            await Navigation.PopModalAsync();
+        }
+
+        /// <summary>
+        /// Do not respond to Android back button
+        /// </summary>
+        /// <returns></returns>
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
+    }
+}
