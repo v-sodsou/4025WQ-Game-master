96eb75457afbb9ea4a9c0b02ad1adc6f4746cd54
Pedro De Almeida Voto <dealmeidavop@seattleu.edu>
2020-02-10 13:07:43 -0800

    Adding CharacterDeletePage.xaml

---
+++ Game/Game/Views/Characters/CharacterDeletePage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Game.Models;
+using Game.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Game.Views.Characters
+{
+    [DesignTimeVisible(false)]
+    public partial class CharacterDeletePage : ContentPage
+    {
+        readonly GenericViewModel<Character> viewModel;
+        public CharacterDeletePage()
+        {
+            InitializeComponent();
+        }
+
+        public CharacterDeletePage(GenericViewModel<Character> data)
+        {
+            InitializeComponent();
+
+            BindingContext = this.viewModel = data;
+
+            this.viewModel.Title = "Delete " + data.Title;
+        }
+    }
+}
