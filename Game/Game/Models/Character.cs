c829e2c6aef78bc65268eabdf2a1f6630c58f832
Pedro De Almeida Voto <dealmeidavop@seattleu.edu>
2020-02-10 12:36:19 -0800

    Adding Attributes to Character.cs and Binding them on CharacterReadPage.xaml

--- Game/Game/Models/Character.cs
+++ Game/Game/Models/Character.cs
@@ -1,9 +1,33 @@
-﻿namespace Game.Models
+﻿using Game.Services;
+
+namespace Game.Models
 {
    
     public class Character : BaseModel<Character>
     {
-       
-        
+        public Character()
+        {
+            ImageURI = CharacterService.DefaultImageURI;
+        }
+
+        public Character(Character data)
+        {
+            Update(data);
+        }
+
+        public override void Update(Character newData)
+        {
+            if (newData == null)
+            {
+                return;
+            }
+
+            // Update all the fields in the Data, except for the Id and guid
+            Name = newData.Name;
+            Description = newData.Description;
+            Name = newData.Name;
+            Description = newData.Description;
+            ImageURI = newData.ImageURI;
+        }
     }
 }
