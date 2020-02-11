ab1f14155c327a4b7f6679a54939619a63d6a19d
sonali dsouza <sonali.18feb@gmail.com>
2020-02-10 23:36:26 -0800

    Added Character Default data in the DefaultData.cs file

--- Game/Game/Services/DefaultData.cs
+++ Game/Game/Services/DefaultData.cs
@@ -64,5 +64,49 @@ namespace Game.Services
 
             return datalist;
         }
+
+        public static List<Character> LoadData(Character temp)
+        {
+            var datalist = new List<Character>()
+            {
+                new Character {
+                    Name = "Princess Leia",
+                    Description = "Member of the Imperial Senate",
+                    ImageURI = "https://cdn.webshopapp.com/shops/209525/files/309648135/funko-pop-star-wars-307-rey.jpg"
+                },
+
+                new Character {
+                    Name = "Luke",
+                    Description = "An incredible Jedi",
+                    ImageURI = "https://images-na.ssl-images-amazon.com/images/I/51-B25E3vTL._AC_SY450_.jpg"
+                },
+
+                new Character {
+                    Name = "Finn",
+                    Description = "Force sensitive stormtrooper",
+                    ImageURI = "https://cdn10.bigcommerce.com/s-2znav/products/1463/images/4353/Finn_75176_with_blaster__82235.1554907493.1280.1280.jpg?c=2"
+                },
+
+                new Character {
+                    Name = "Ahsoka Tano",
+                    Description = "A Togruta Jedi Padawan",
+                    ImageURI = "https://mygenerationtoys.com/images/Star%20Wars%20Galactic%20Heroes%20Ahsoka%20Tano.jpg"
+                },
+
+                new Character {
+                    Name = "Chewbacca",
+                    Description = "A tall, hirsute, and intelligent species",
+                    ImageURI = "https://www.toysrus.ca/dw/image/v2/BDFX_PRD/on/demandware.static/-/Sites-toys-master-catalog/default/dwcda6d643/images/AFABDCFC_1.jpg?sw=767&sh=767&sm=fit"
+                },
+
+                new Character {
+                    Name = "Obi-Wan Kenobi",
+                    Description = "A legendary Jedi Master",
+                    ImageURI = "https://images-na.ssl-images-amazon.com/images/I/51X10mZk4ML._AC_SL1082_.jpg"
+                }
+            };
+
+            return datalist;
+        }
     }
 }
