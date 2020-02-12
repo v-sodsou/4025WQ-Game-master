c000084943775804f621bc105435be5f23852978
sonali dsouza <sonali.18feb@gmail.com>
2020-02-11 22:39:31 -0800

    Added MonsterService class

---
+++ Game/Game/Services/MonsterService.cs
@@ -0,0 +1,8 @@
+﻿namespace Game.Services
+{
+    static class MonsterService
+    {
+        // Return the Default Image URI for the Local Image for a Monster.
+        public static string DefaultImageURI = "icon_new.png";
+    }
+}
