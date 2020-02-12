49c472109319203c3f3bd07b8695d5a4ef2353b9
sonali dsouza <sonali.18feb@gmail.com>
2020-02-11 22:35:47 -0800

    Added MonsterIndexPage

---
+++ Game/Game/Views/Monsters/MonsterIndexPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Game.Models;
+using Game.ViewModels;
+using Game.Views.Characters;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Index Page
+    /// </summary>
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0019:Use pattern matching", Justification = "<Pending>")]
+    [DesignTimeVisible(false)]
+    public partial class MonsterIndexPage : ContentPage
+    {
+        // The view model, used for data binding
+        readonly MonsterIndexViewModel ViewModel;
+
+       
+        public MonsterIndexPage()
+        {
+            InitializeComponent();
+
+            BindingContext = ViewModel = MonsterIndexViewModel.Instance;
+        }
+
+        
+        /// <summary>
+        /// Refresh the list on page appearing
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = null;
+
+            // If no data, then set it for needing refresh
+            if (ViewModel.Dataset.Count == 0)
+            {
+                ViewModel.SetNeedsRefresh(true);
+            }
+
+            // If the needs Refresh flag is set update it
+            if (ViewModel.NeedsRefresh())
+            {
+                ViewModel.LoadDatasetCommand.Execute(null);
+            }
+
+            BindingContext = ViewModel;
+        }
+    }
+}
