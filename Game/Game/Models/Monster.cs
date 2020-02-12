639809826d5bedef12b5516375a6b22753693fb7
sonali dsouza <sonali.18feb@gmail.com>
2020-02-11 22:40:32 -0800

    Added Monster class

---
+++ Game/Game/Models/Monster.cs
@@ -0,0 +1,37 @@
+﻿using Game.Services;
+
+namespace Game.Models
+{
+   
+    public class Monster : BaseModel<Monster>
+    {
+        public Monster()
+        {
+            ImageURI = MonsterService.DefaultImageURI;
+            HasForce = false;
+        }
+
+        public Monster(Monster data)
+        {
+            Update(data);
+        }
+
+        public bool HasForce { get; set; }
+
+        public override void Update(Monster newData)
+        {
+            if (newData == null)
+            {
+                return;
+            }
+
+            // Update all the fields in the Data, except for the Id and guid
+            Name = newData.Name;
+            Description = newData.Description;
+            Name = newData.Name;
+            Description = newData.Description;
+            ImageURI = newData.ImageURI;
+            HasForce = newData.HasForce;
+        }
+    }
+}
