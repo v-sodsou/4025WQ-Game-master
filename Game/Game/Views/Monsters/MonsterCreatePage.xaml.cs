f32a7e1c942dec42170297a57689c4f4214571b5
sonali dsouza <sonali.18feb@gmail.com>
2020-02-11 23:15:06 -0800

    Added MonsterCreatePage

---
+++ Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Game.Models;
+using Game.ViewModels;
+using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Game.Views
+{
+    /// <summary>
+    /// Create Character
+    /// </summary>
+    [DesignTimeVisible(false)]
+    public partial class MonsterCreatePage : ContentPage
+    {
+        // The character to create
+        GenericViewModel<Monster> ViewModel { get; set; }
+
+        /// <summary>
+        /// Constructor for Create makes a new model
+        /// </summary>
+        public MonsterCreatePage(GenericViewModel<Monster> data)
+        {
+            InitializeComponent();
+
+            data.Data = new Monster();
+
+            BindingContext = this.ViewModel = data;
+
+            this.ViewModel.Title = "Create Monster";
+
+        }
+
+       
+
+
+    }
+}
