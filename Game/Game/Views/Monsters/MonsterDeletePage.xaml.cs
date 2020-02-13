9abb62b90845846dd04d8b53ee391008ba5ae7c3
maencina <mencina@cityuniversity.edu>
2020-02-12 21:29:59 -0800

    Added functionality to delete monster

--- Game/Game/Views/Monsters/MonsterDeletePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterDeletePage.xaml.cs
@@ -14,13 +14,23 @@ namespace Game.Views.Monsters
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MonsterDeletePage : ContentPage
     {
+        /// <summary>
+        /// Monster view model
+        /// </summary>
         readonly GenericViewModel<MonsterModel> viewModel;
 
-        public MonsterDeletePage(GenericViewModel<CharacterModel> data)
+        /// <summary>
+        /// Parameterless Constructor
+        /// </summary>
+        public MonsterDeletePage()
         {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Constructor MonsterDeletePage
+        /// </summary>
+        /// <param name="data"></param>
         public MonsterDeletePage(GenericViewModel<MonsterModel> data)
         {
             InitializeComponent();
@@ -30,17 +40,31 @@ namespace Game.Views.Monsters
             this.viewModel.Title = "Delete " + data.Title;
         }
 
+        /// <summary>
+        /// MonsterDelete click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         async void MonsterDelete_Clicked(object sender, EventArgs e)
         {
             MessagingCenter.Send(this, "Delete", viewModel.Data);
             await Navigation.PopModalAsync();
         }
 
+        /// <summary>
+        /// Monster Cancel Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         async void MonsterCancel_Clicked(object sender, EventArgs e)
         {
             await Navigation.PopModalAsync();
         }
 
+        /// <summary>
+        /// Fix issue with Android back button
+        /// </summary>
+        /// <returns></returns>
         protected override bool OnBackButtonPressed()
         {
             return true;
