098187751e6c3664b33d8d82e9225741eba1a4ee
maencina <mencina@cityuniversity.edu>
2020-02-13 23:38:28 -0800

    Added comments to Monster Read Page

--- Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
@@ -14,19 +14,36 @@ namespace Game.Views.Monsters
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MonsterReadPage : ContentPage
     {
+        /// <summary>
+        /// view model variable
+        /// </summary>
         readonly GenericViewModel<MonsterModel> ViewModel;
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="data"></param>
         public MonsterReadPage(GenericViewModel<MonsterModel> data)
         {
             InitializeComponent();
             BindingContext = this.ViewModel = data;
         }
 
+        /// <summary>
+        /// Edit Monster Click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         async void EditMonster_Clicked(object sender, System.EventArgs e)
         {
             await Navigation.PushModalAsync(new NavigationPage(new MonsterUpdatePage(new GenericViewModel<MonsterModel>(ViewModel.Data))));
             await Navigation.PopAsync();
         }
 
+        /// <summary>
+        /// Delete Monster Click event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         async void DeleteMonster_Clicked(object sender, System.EventArgs e)
         {
             await Navigation.PushModalAsync(new NavigationPage(new MonsterDeletePage(new GenericViewModel<MonsterModel>(ViewModel.Data))));
