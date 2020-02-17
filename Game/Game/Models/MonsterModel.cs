f2d0b45d6232d8bb488f3e4e1e26ddee9324b319
sonali dsouza <sonali.18feb@gmail.com>
2020-02-17 12:50:56 -0800

    Add comments to Monster Model attributes

--- Game/Game/Models/MonsterModel.cs
+++ Game/Game/Models/MonsterModel.cs
@@ -6,13 +6,29 @@ namespace Game.Models
     public class MonsterModel : BaseModel<MonsterModel>
     {
         // Attributes
+
+        //Attack of the monster
         public int Attack { get; set; }
+
+        //Defence of the monster
         public int Defense { get; set; }
+
+        //Current health of the monster
         public int Health { get; set; }
+
+        //Speed of the monster
         public int Speed { get; set; }
+
+        // Amount of experience the monster has
         public int Experience { get; set; }
+
+
         public int id { get; set; }
+
+        // Current level of the monster
         public int Level { get; set; }
+
+        // Flag indicating whether a Monster has force special ability
         public bool HasForce { get; set; }
 
 
