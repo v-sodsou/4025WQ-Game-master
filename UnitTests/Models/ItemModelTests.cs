58bd960021490ba2c450c9b995d1f1769ec0e1ff
sonali dsouza <sonali.18feb@gmail.com>
2020-02-22 15:33:27 -0800

    Added ItemModel_Constructor_New_Item_Should_Copy test in ItemModelsTest

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -1,11 +1,7 @@
 ﻿using NUnit.Framework;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using Game.Models;
+using Game.Helpers;
 
 namespace UnitTests.Models
 {
@@ -19,11 +15,29 @@ namespace UnitTests.Models
 
             // Act
             var result = new ItemModel();
-            
+
             // Reset
 
-            // Assert
+            // Assert 
             Assert.IsNotNull(result);
         }
+
+        [Test]
+        public void ItemModel_Constructor_New_Item_Should_Copy()
+        {
+            // Arrange
+            var dataNew = new ItemModel();
+            dataNew.Value = 2;
+            dataNew.Id = "oldID";
+
+            // Act
+            var result = new ItemModel(dataNew);
+
+            // Reset
+
+            // Assert 
+            Assert.AreNotEqual("oldID", result.Id);
+        }
+
     }
 }
