78f1e274491be0ec12c116549679b27f7634051e
sonali dsouza <sonali.18feb@gmail.com>
2020-02-22 23:12:26 -0800

    Add TearDown and BasePlayerModel_Constructor_Default_Should_Pass test in BasePlayerModelTests

--- UnitTests/Models/BasePlayerModelTests.cs
+++ UnitTests/Models/BasePlayerModelTests.cs
@@ -1,12 +1,33 @@
-﻿using NUnit.Framework;
+﻿using Game.Models;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace UnitTests.Models
 {
     [TestFixture]
     class BasePlayerModelTests
     {
+        [TearDown]
+        public async Task TearDown()
+        {
+            await Game.Helpers.DataSetsHelper.WipeData();
+        }
+
+        [Test]
+        public void BasePlayerModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new BasePlayerModel<CharacterModel>();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Enter a name here...", result.Name);
+        }
     }
 }
