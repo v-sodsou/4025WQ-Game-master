a085a7fe30dd4d5c966141a8c30c15c49863f4ee
sonali dsouza <sonali.18feb@gmail.com>
2020-02-22 22:49:02 -0800

    Added BaseModel_Constructor_Default_Should_Pass test in BaseModelTests class

--- UnitTests/Models/BaseModelTests.cs
+++ UnitTests/Models/BaseModelTests.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Game.Models;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UnitTests.Models
 {
+    [TestFixture]
     class BaseModelTests
     {
+        [Test]
+        public void BaseModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new BaseModel<ItemModel>();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Enter a name here...", result.Name);
+        }
+
     }
 }
