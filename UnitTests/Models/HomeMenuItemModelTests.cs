6c317577dbb0348e5b59509cb5b9f73ceee2aaf9
sonali dsouza <sonali.18feb@gmail.com>
2020-02-22 23:05:41 -0800

    Added HomeMenuItemModel_Constructor_Default_Should_Pass test in HomeMenuItemModelTests

---
+++ UnitTests/Models/HomeMenuItemModelTests.cs
@@ -0,0 +1,26 @@
+﻿using Game.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    class HomeMenuItemModelTests
+    {
+        [Test]
+        public void HomeMenuItemModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new HomeMenuItemModel();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
