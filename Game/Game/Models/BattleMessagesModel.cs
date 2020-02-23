775885669fd2eeb4c4589e3af2947e0a4c7029d4
sonali dsouza <sonali.18feb@gmail.com>
2020-02-23 15:53:42 -0800

    Added BattleMessagesModel properties

--- Game/Game/Models/BattleMessagesModel.cs
+++ Game/Game/Models/BattleMessagesModel.cs
@@ -6,5 +6,40 @@ namespace Game.Models
 {
     class BattleMessagesModel
     {
+        // Is the player a character or a monster
+        public PlayerTypeEnum PlayerType = PlayerTypeEnum.Unknown;
+
+        // The Status of the action
+        public HitStatusEnum HitStatus = HitStatusEnum.Unknown;
+
+        // Name of the Attacker
+        public string AttackerName = string.Empty;
+
+        // Name of who the target was
+        public string TargetName = string.Empty;
+
+        // The status of the Attack
+        public string AttackStatus = string.Empty;
+
+        // Turn Message
+        public string TurnMessage = string.Empty;
+
+        // Turn Special Message
+        public string TurnMessageSpecial = string.Empty;
+
+        // Level Up Message
+        public string LevelUpMessage = string.Empty;
+
+        // Amount of Damage
+        public int DamageAmount = 0;
+
+        // The Remaining Health Mesage
+        public int CurrentHealth = 0;
+
+        // Beginning of the Html Block for html formatting
+        public string htmlHead = @"<html><body bgcolor=""#E8D0B6""><p>";
+
+        // Ending of the Html Block for Html formatting
+        public string htmlTail = @"</p></body></html>";
     }
 }
