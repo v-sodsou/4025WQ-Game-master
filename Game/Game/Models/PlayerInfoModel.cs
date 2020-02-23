a12ffd91c2d5ce1f71f9c204e68329a7b9d7a147
sonali dsouza <sonali.18feb@gmail.com>
2020-02-22 23:52:17 -0800

    Added PlayerInfoModel class

---
+++ Game/Game/Models/PlayerInfoModel.cs
@@ -0,0 +1,99 @@
+﻿
+namespace Game.Models
+{
+    /// <summary>
+    /// Player for the game.
+    /// 
+    /// Either Monster or Character
+    /// 
+    /// Constructor Player to Player used a T in Round
+    /// </summary>
+    public class PlayerInfoModel : BasePlayerModel<PlayerInfoModel>
+    {
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public PlayerInfoModel() { }
+
+        /// <summary>
+        /// Copy from one PlayerInfoModel into Another
+        /// </summary>
+        /// <param name="data"></param>
+        public PlayerInfoModel(PlayerInfoModel data)
+        {
+            PlayerType = data.PlayerType;
+            Guid = data.Guid;
+            Alive = data.Alive;
+            ExperiencePoints = data.ExperienceTotal;
+            Level = data.Level;
+            Name = data.Name;
+            Description = data.Description;
+            Speed = data.GetSpeed();
+            ImageURI = data.ImageURI;
+            CurrentHealth = data.GetCurrentHealthTotal;
+            MaxHealth = data.GetMaxHealthTotal;
+
+            // Set the strings for the items
+            Head = data.Head;
+            Feet = data.Feet;
+            Necklass = data.Necklass;
+            RightFinger = data.RightFinger;
+            LeftFinger = data.LeftFinger;
+            Feet = data.Feet;
+        }
+
+        /// <summary>
+        /// Create PlayerInfoModel from character
+        /// </summary>
+        /// <param name="data"></param>
+        public PlayerInfoModel(CharacterModel data)
+        {
+            PlayerType = data.PlayerType;
+            Guid = data.Guid;
+            Alive = data.Alive;
+            ExperiencePoints = data.ExperienceTotal;
+            Level = data.Level;
+            Name = data.Name;
+            Description = data.Description;
+            Speed = data.GetSpeed();
+            ImageURI = data.ImageURI;
+            CurrentHealth = data.GetCurrentHealthTotal;
+            MaxHealth = data.GetMaxHealthTotal;
+
+            // Set the strings for the items
+            Head = data.Head;
+            Feet = data.Feet;
+            Necklass = data.Necklass;
+            RightFinger = data.RightFinger;
+            LeftFinger = data.LeftFinger;
+            Feet = data.Feet;
+        }
+
+        /// <summary>
+        /// Crate PlayerInfoModel from Monster
+        /// </summary>
+        /// <param name="data"></param>
+        public PlayerInfoModel(MonsterModel data)
+        {
+            PlayerType = data.PlayerType;
+            Guid = data.Guid;
+            Alive = data.Alive;
+            ExperiencePoints = data.ExperienceTotal;
+            Level = data.Level;
+            Name = data.Name;
+            Description = data.Description;
+            Speed = data.GetSpeed();
+            ImageURI = data.ImageURI;
+            CurrentHealth = data.GetCurrentHealthTotal;
+            MaxHealth = data.GetMaxHealthTotal;
+
+            // Set the strings for the items
+            Head = data.Head;
+            Feet = data.Feet;
+            Necklass = data.Necklass;
+            RightFinger = data.RightFinger;
+            LeftFinger = data.LeftFinger;
+            Feet = data.Feet;
+        }
+    }
+}
