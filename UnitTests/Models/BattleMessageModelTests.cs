46058da6151ee95873cc4bb4eff5f014b5825634
maencina <mencina@cityuniversity.edu>
2020-02-23 17:06:39 -0800

    Added BattleMessageModelTests Class and construtor UT.

---
+++ UnitTests/Models/BattleMessageModelTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+using Game.Models;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    class BattleMessageModelTests
+    {
+        [Test]
+        public void BattleMessageModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new BattleMessagesModel();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
