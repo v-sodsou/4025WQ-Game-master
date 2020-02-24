e796242cbbb5fc439728b4a451f64de0fa3a8bd7
Pedro De Almeida Voto <dealmeidavop@seattleu.edu>
2020-02-23 19:25:35 -0800

    Added ItemIndexViewModel_Read_Invalid_ID_Bogus_Should_Fail, ItemIndexViewModel_Constructor_Default_Should_Pass, ItemIndexViewModel_SortDataSet_Default_Should_Pass

--- UnitTests/ViewModels/ItemIndexViewModelTests.cs
+++ UnitTests/ViewModels/ItemIndexViewModelTests.cs
@@ -4,6 +4,8 @@ using Xamarin.Forms.Mocks;
 using Xamarin.Forms;
 using Game.Services;
 using System.Threading.Tasks;
+using Game.Models;
+using System.Collections.Generic;
 
 namespace UnitTests.ViewModels
 {
@@ -29,6 +31,56 @@ namespace UnitTests.ViewModels
         public async Task ResetDataAsync()
         {
             await ViewModel.WipeDataListAsync();
+        }
+
+        [Test]
+        public async Task ItemIndexViewModel_Read_Invalid_ID_Bogus_Should_Fail()
+        {
+            // Arrange
+
+            // Act
+            var result = await ViewModel.ReadAsync("bogus");
+
+            // Reset
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void ItemIndexViewModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = ViewModel;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void ItemIndexViewModel_SortDataSet_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Add items into the list Z ordered
+            var dataList = new List<ItemModel>();
+            dataList.Add(new ItemModel { Name = "z" });
+            dataList.Add(new ItemModel { Name = "m" });
+            dataList.Add(new ItemModel { Name = "a" });
+
+            // Act
+            var result = ViewModel.SortDataset(dataList);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("a", result[0].Name);
+            Assert.AreEqual("m", result[1].Name);
+            Assert.AreEqual("z", result[2].Name);
         }
     }
 }
