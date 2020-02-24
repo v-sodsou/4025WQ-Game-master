92e620f724f5f8f58ccac15b54eff3ea30d09a1a
Pedro De Almeida Voto <dealmeidavop@seattleu.edu>
2020-02-23 18:54:51 -0800

    Added BaseViewModelTests.cs

---
+++ UnitTests/ViewModels/BaseViewModelTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Game.ViewModels;
+using Game.Models;
+
+namespace UnitTests.ViewModels
+{
+    [TestFixture]
+    public class BaseViewModelTests : BaseViewModel<ItemModel>
+    {
+        BaseViewModel<ItemModel> ViewModel;
+    }
+}
