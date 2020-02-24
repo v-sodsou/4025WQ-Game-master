8f42743c77853a417c1474c4a6ce6dace476c245
Pedro De Almeida Voto <dealmeidavop@seattleu.edu>
2020-02-23 17:57:51 -0800

    Added ItemLocationEnumExtensionsTests_Head_Default_Should_Pass,  ItemLocationEnumExtensionsTests_Necklass_Default_Should_Pass, and ItemLocationEnumExtensionsTests_PrimaryHand_Default_Should_Pass

--- UnitTests/Models/ItemLocationEnumExtensionsTests.cs
+++ UnitTests/Models/ItemLocationEnumExtensionsTests.cs
@@ -20,5 +20,47 @@ namespace UnitTests.Models
             // Assert
             Assert.AreEqual("Unknown", result);
         }
+
+        [Test]
+        public void ItemLocationEnumExtensionsTests_Head_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = ItemLocationEnum.Head.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Head", result);
+        }
+
+        [Test]
+        public void ItemLocationEnumExtensionsTests_Necklass_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = ItemLocationEnum.Necklass.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Necklass", result);
+        }
+
+        [Test]
+        public void ItemLocationEnumExtensionsTests_PrimaryHand_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = ItemLocationEnum.PrimaryHand.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Primary Hand", result);
+        }
     }
 }
