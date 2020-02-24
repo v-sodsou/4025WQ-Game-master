b9acca365f2e0a6bc0f2d4624d780ef25ac50adb
maencina <mencina@cityuniversity.edu>
2020-02-23 16:06:30 -0800

    Added UT to Base Player Model Set default

--- UnitTests/Models/BasePlayerModelTests.cs
+++ UnitTests/Models/BasePlayerModelTests.cs
@@ -68,6 +68,63 @@ namespace UnitTests.Models
         }
 
         [Test]
+        public void BasePlayerModel_Set_Get_Default_Should_Pass()
+        {
+            // Arrange
+            var result = new BasePlayerModel<CharacterModel>();
+
+            // Act
+            result.Id = "bogus";
+            result.ImageURI = "uri";
+            result.PlayerType = PlayerTypeEnum.Monster;
+            result.Alive = false;
+            result.Order = 100;
+            result.Guid = "guid";
+            result.ListOrder = 200;
+            result.Speed = 300;
+            result.Level = 400;
+            result.ExperiencePoints = 500;
+            result.CurrentHealth = 600;
+            result.MaxHealth = 700;
+            result.ExperienceTotal = 800;
+            result.Defense = 900;
+            result.Attack = 123;
+            result.Head = "head";
+            result.Feet = "feet";
+            result.Necklass = "necklass";
+            result.PrimaryHand = "primaryhand";
+            result.OffHand = "offhand";
+            result.RightFinger = "rightfinger";
+            result.LeftFinger = "leftfinger";
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("bogus", result.Id);
+            Assert.AreEqual("uri", result.ImageURI);
+            Assert.AreEqual(PlayerTypeEnum.Monster, result.PlayerType);
+            Assert.AreEqual(false, result.Alive);
+            Assert.AreEqual(100, result.Order);
+            Assert.AreEqual("guid", result.Guid);
+            Assert.AreEqual(200, result.ListOrder);
+            Assert.AreEqual(300, result.Speed);
+            Assert.AreEqual(400, result.Level);
+            Assert.AreEqual(500, result.ExperiencePoints);
+            Assert.AreEqual(600, result.CurrentHealth);
+            Assert.AreEqual(700, result.MaxHealth);
+            Assert.AreEqual(800, result.ExperienceTotal);
+            Assert.AreEqual(900, result.Defense);
+            Assert.AreEqual(123, result.Attack);
+            Assert.AreEqual("head", result.Head);
+            Assert.AreEqual("feet", result.Feet);
+            Assert.AreEqual("necklass", result.Necklass);
+            Assert.AreEqual("primaryhand", result.PrimaryHand);
+            Assert.AreEqual("offhand", result.OffHand);
+            Assert.AreEqual("rightfinger", result.RightFinger);
+            Assert.AreEqual("leftfinger", result.LeftFinger);
+        }
+
+        [Test]
         public void BasePlayerModel_GetItemByLocation_Head_Default_Should_Pass()
         {
             // Arrange
