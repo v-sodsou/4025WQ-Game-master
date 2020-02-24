ad1308d84c99a61eea82836b426227be05243efd
Pedro De Almeida Voto <dealmeidavop@seattleu.edu>
2020-02-23 19:12:40 -0800

    Added GenericViewModelTests

---
+++ UnitTests/ViewModels/GenericViewModelTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Game.Models;
+using Game.ViewModels;
+
+namespace UnitTests.ViewModels
+{
+    [TestFixture]
+    public class GenericViewModelTests
+    {
+        
+    }
+}
