38dcede244bbf091340a20b8fc1809b9c50cf56d
sonali dsouza <sonali.18feb@gmail.com>
2020-02-23 16:09:12 -0800

    Added RollToHitTarget method in TurnEngine class

--- Game/Game/Engine/TurnEngine.cs
+++ Game/Game/Engine/TurnEngine.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Game.Helpers;
+using Game.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,55 @@ namespace Game.Engine
 {
     class TurnEngine: BaseEngine
     {
+
+        #region Algrorithm
+        // Attack or Move
+        // Roll To Hit
+        // Decide Hit or Miss
+        // Decide Damage
+        // Death
+        // Drop Items
+        // Turn Over
+        #endregion Algrorithm
+
+
+        /// <summary>
+        /// Roll To Hit
+        /// </summary>
+        /// <param name="AttackScore"></param>
+        /// <param name="DefenseScore"></param>
+        /// <returns></returns>
+        public HitStatusEnum RollToHitTarget(int AttackScore, int DefenseScore)
+        {
+            var d20 = DiceHelper.RollDice(1, 20);
+
+            if (d20 == 1)
+            {
+                // Force Miss
+                BattleMessagesModel.HitStatus = HitStatusEnum.Miss;
+                return BattleMessagesModel.HitStatus;
+            }
+
+            if (d20 == 20)
+            {
+                // Force Hit
+                BattleMessagesModel.HitStatus = HitStatusEnum.Hit;
+                return BattleMessagesModel.HitStatus;
+            }
+
+            var ToHitScore = d20 + AttackScore;
+            if (ToHitScore < DefenseScore)
+            {
+                BattleMessagesModel.AttackStatus = " misses ";
+                // Miss
+                BattleMessagesModel.HitStatus = HitStatusEnum.Miss;
+                BattleMessagesModel.DamageAmount = 0;
+                return BattleMessagesModel.HitStatus;
+            }
+
+            // Hit
+            BattleMessagesModel.HitStatus = HitStatusEnum.Hit;
+            return BattleMessagesModel.HitStatus;
+        }
     }
 }
