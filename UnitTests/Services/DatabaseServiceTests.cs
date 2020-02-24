46a7daf0c82fd864d186020c62e000f7b2592321
Pedro De Almeida Voto <dealmeidavop@seattleu.edu>
2020-02-23 18:16:57 -0800

    Added DatabaseServiceTests.cs

---
+++ UnitTests/Services/DatabaseServiceTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Game.Models;
+using Game.Services;
+
+namespace UnitTests.Services
+{
+    [TestFixture]
+    public class DatabaseServiceTests
+    {
+        DatabaseService<ItemModel> DataStore;
+    }
+}
