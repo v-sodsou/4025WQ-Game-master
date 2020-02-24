c5146180f5e70057e9eebf36c754cfaae07e7476
maencina <mencina@cityuniversity.edu>
2020-02-23 16:35:32 -0800

    Added UT BasePlayerModel FormatOutput

--- UnitTests/Models/BasePlayerModelTests.cs
+++ UnitTests/Models/BasePlayerModelTests.cs
@@ -215,6 +215,21 @@ namespace UnitTests.Models
         }
 
         [Test]
+        public void BasePlayerModel_FormatOutput_Default_Should_Pass()
+        {
+            // Arrange
+            var data = new BasePlayerModel<CharacterModel>();
+
+            // Act
+            var result = data.FormatOutput();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("", result);
+        }
+
+        [Test]
         public void BasePlayerModel_GetItemByLocation_Head_Default_Should_Pass()
         {
             // Arrange
