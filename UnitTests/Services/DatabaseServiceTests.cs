8004a9aec4dfcf8642444c038281cc57fef23545
Pedro De Almeida Voto <dealmeidavop@seattleu.edu>
2020-02-23 18:18:01 -0800

    Added Setup, and TearDown

--- UnitTests/Services/DatabaseServiceTests.cs
+++ UnitTests/Services/DatabaseServiceTests.cs
@@ -11,5 +11,19 @@ namespace UnitTests.Services
     public class DatabaseServiceTests
     {
         DatabaseService<ItemModel> DataStore;
+
+        [SetUp]
+        public void Setup()
+        {
+            //DatabaseService<ItemModel>.TestMode = true;
+            DatabaseService<ItemModel>.TestMode = true;
+            DataStore = DatabaseService<ItemModel>.Instance;
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await DataStore.WipeDataListAsync();
+        }
     }
 }
