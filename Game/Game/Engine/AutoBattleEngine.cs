2d3eabb31a986ac0c448386d49be10047edb250f
sonali dsouza <sonali.18feb@gmail.com>
2020-02-23 19:14:19 -0800

    Added GetScoreObject method in AutoBattleEngine

--- Game/Game/Engine/AutoBattleEngine.cs
+++ Game/Game/Engine/AutoBattleEngine.cs
@@ -1,10 +1,38 @@
-﻿using System;
+﻿using Game.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Game.Engine
 {
-    class AutoBattleEngine
+    class AutoBattleEngine: BattleEngine
     {
+
+        #region Algrorithm
+        // Prepare for Battle
+        // Pick 6 Characters
+        // Initialize the Battle
+        // Start a Round
+
+        // Fight Loop
+        // Loop in the round each turn
+        // If Round is over, Start New Round
+        // Check end state of round/game
+
+        // Wrap up
+        // Get Score
+        // Save Score
+        // Output Score
+        #endregion Algrorithm
+
+        // The Battle Engine
+        // BattleEngine Engine = new BattleEngine();
+
+        /// <summary>
+        /// Return the Score Object
+        /// </summary>
+        /// <returns></returns>
+        public ScoreModel GetScoreObject() { return BattleScore; }
+
     }
 }
