120fd04ad208b03b6a365703a393b7ae2420d53f
Pedro De Almeida Voto <dealmeidavop@seattleu.edu>
2020-02-23 17:56:24 -0800

    Added ItemLocationEnumExtensions

--- UnitTests/Models/ItemLocationEnumExtensionsTests.cs
+++ UnitTests/Models/ItemLocationEnumExtensionsTests.cs
@@ -1,10 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
+
+using Game.Models;
 
 namespace UnitTests.Models
 {
+    [TestFixture]
     class ItemLocationEnumExtensionsTests
     {
+        [Test]
+        public void ItemLocationEnumExtensionsTests_Unknown_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = ItemLocationEnum.Unknown.ToMessage();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Unknown", result);
+        }
     }
 }
