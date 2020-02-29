7807a0a140a7a731068f47685164a3423d232a60
sonali dsouza <sonali.18feb@gmail.com>
2020-02-29 15:09:29 -0800

    Added Difficulty Types in DifficultyEnum cs file

--- Game/Game/Models/Enum/DifficultyEnum.cs
+++ Game/Game/Models/Enum/DifficultyEnum.cs
@@ -1,10 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace Game.Models.Enum
+namespace Game.Models
 {
-    class DifficultyEnum
+    /// <summary>
+    /// The Types of Difficultys
+    /// Used by Item to specify what it modifies.
+    /// </summary>
+    public enum DifficultyEnum
     {
+        // Not specified
+        Unknown = 0,
+
+        // Easier than mostThe speed of the character, impacts movement, and initiative
+        Easy = 10,
+
+        // Average
+        Average = 12,
+
+        // Hard
+        Hard = 14,
+
+        // Harder than Hard
+        Difficult = 16,
+
+        // The highest value
+        Impossible = 18,
     }
+
+   
 }
