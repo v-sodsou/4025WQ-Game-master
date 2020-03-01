be1e974fad4973d984b62d7946a2bd7117287509
sonali dsouza <sonali.18feb@gmail.com>
2020-02-29 18:05:00 -0800

    Added Update method in Monster model

--- Game/Game/Models/MonsterModel.cs
+++ Game/Game/Models/MonsterModel.cs
@@ -25,5 +25,50 @@ namespace Game.Models
             Level = 1;
         }
 
+        /// <summary>
+        /// Make a copy
+        /// </summary>
+        /// <param name="data"></param>
+        public MonsterModel(MonsterModel data)
+        {
+            Update(data);
+        }
+
+        /// <summary>
+        /// Update
+        /// </summary>
+        /// <param name="newData"></param>
+        /// <returns></returns>
+        public override bool Update(MonsterModel newData)
+        {
+            if (newData == null)
+            {
+                return false;
+            }
+
+            PlayerType = newData.PlayerType;
+            Guid = newData.Guid;
+            Name = newData.Name;
+            Description = newData.Description;
+            Level = newData.Level;
+            ImageURI = newData.ImageURI;
+
+            Difficulty = newData.Difficulty;
+
+            Speed = newData.Speed;
+            Defense = newData.Defense;
+            Attack = newData.Attack;
+
+            ExperienceTotal = newData.ExperienceTotal;
+            ExperienceRemaining = newData.ExperienceRemaining;
+            CurrentHealth = newData.CurrentHealth;
+            MaxHealth = newData.MaxHealth;
+
+            UniqueItem = newData.UniqueItem;
+            HasForce = newData.HasForce;
+
+            return true;
+        }
+
     }
 }
