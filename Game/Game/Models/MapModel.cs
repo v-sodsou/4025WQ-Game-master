6ae83b4105c52f0387725647fc8c1e1d5de401ce
maencina <mencina@cityuniversity.edu>
2020-03-18 10:42:12 -0700

    Added code to create an empty map

--- Game/Game/Models/MapModel.cs
+++ Game/Game/Models/MapModel.cs
@@ -25,6 +25,26 @@ namespace Game.Models
             // Create the Map
             MapGridLocation = new MapModelLocation[MapXAxiesCount, MapYAxiesCount];
 
+            ClearMapGrid();
+
+        }
+
+        /// <summary>
+        /// Create an Empty Map
+        /// </summary>
+        /// <returns></returns>
+        public bool ClearMapGrid()
+        {
+            //Populate Map with Empty Values
+            for (var x = 0; x < MapXAxiesCount; x++)
+            {
+                for (var y = 0; y < MapYAxiesCount; y++)
+                {
+                    // Populate the entire map with blank
+                    MapGridLocation[x, y] = new MapModelLocation { Row = y, Column = x, Player = EmptySquare };
+                }
+            }
+            return true;
         }
 
     }
--- Game/Game/Models/MapModelLocation.cs
+++ Game/Game/Models/MapModelLocation.cs
@@ -6,6 +6,16 @@ namespace Game.Models
 {
     public class MapModelLocation
     {
+        // The X, which is R in Grid
+        public int Row;
 
+        // They Y, which is Column in Grid
+        public int Column;
+
+        // The Player, Character or Unknown for blank
+        public PlayerInfoModel Player = new PlayerInfoModel();
+
+        // If IsSelected, used for targeting
+        public bool IsSelectedTarget = false;
     }
 }
