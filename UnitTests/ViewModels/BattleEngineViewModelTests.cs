4a56a2da463ca108cfbe179e4bce78ff8dec9e9d
Pedro De Almeida Voto <dealmeidavop@seattleu.edu>
2020-03-19 20:15:34 -0700

    Adding unit test BattleEngineViewModel Constructor

---
+++ UnitTests/ViewModels/BattleEngineViewModelTests.cs
@@ -0,0 +1,37 @@
+﻿using Game.ViewModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.Mocks;
+
+namespace UnitTests.ViewModels
+{
+    class BattleEngineViewModelTests
+    {
+        BattleEngineViewModel ViewModel;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            ViewModel = BattleEngineViewModel.Instance;
+        }
+
+        [Test]
+        public void BattleEngineViewModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = ViewModel;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
