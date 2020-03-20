f3ab2ec9c69a4df02cbdb2604edc3fbcac04e3a0
Pedro De Almeida Voto <dealmeidavop@seattleu.edu>
2020-03-19 23:42:00 -0700

    Adding Unit tests Battle Page Test

---
+++ UnitTests/Views/Battle/BattlePageTests.cs
@@ -0,0 +1,191 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Game;
+using Game.Views;
+using Xamarin.Forms.Mocks;
+using Xamarin.Forms;
+using Game.Models;
+using Game.ViewModels;
+
+namespace UnitTests.Views
+{
+    [TestFixture]
+    public class BattlePageTests
+    {
+        App app;
+        BattlePage page;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new BattlePage();
+
+            // Put seed data into the system for all tests
+            page.EngineViewModel.Engine.CharacterList.Add(new PlayerInfoModel(new CharacterModel()));
+            page.EngineViewModel.Engine.MonsterList.Add(new PlayerInfoModel(new MonsterModel()));
+            page.EngineViewModel.Engine.MakePlayerList();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+        //[Test]
+        //public void BattlePage_OnAppearing_Should_Pass()
+        //{
+        //    // Get the current valute
+
+        //    // Act
+        //    OnAppearing();
+
+        //    // Reset
+
+        //    // Assert
+        //    Assert.IsTrue(true); // Got to here, so it happened...
+        //}
+
+        [Test]
+        public void BattlePage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void BattlePage_AttackButton_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.AttackButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void BattlePage_ShowScoreButton_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.ShowScoreButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void BattlePage_ExitButton_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.ExitButton_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void BattlePage_ShowModalRoundOverPage_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.ShowModalRoundOverPage();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+
+        [Test]
+        public void BattlePage_ClearMessages_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.ClearMessages();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void BattlePage_GameMessage_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.GameMessage();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void BattlePage_GameMessage_LevelUp_Default_Should_Pass()
+        {
+            // Arrange
+            BattleEngineViewModel.Instance.Engine.BattleMessagesModel.LevelUpMessage = "me";
+
+            // Act
+            page.GameMessage();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+        [Test]
+        public void BattlePage_GameOver_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.GameOver();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+        
+    }
+}
