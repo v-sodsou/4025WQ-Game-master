725d16b3da647603fec69f84d9d73dc62f16217b
Pedro De Almeida Voto <dealmeidavop@seattleu.edu>
2020-03-19 23:37:22 -0700

    Adding BattleScenarioTest

---
+++ UnitTests/Views/Battle/BattlePageScenerioTests.cs
@@ -0,0 +1,55 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
+
+using NUnit.Framework;
+using System.Threading.Tasks;
+using System.Linq;
+
+using Game.Helpers;
+using Game.ViewModels;
+using Game.Views;
+using Game.Engine;
+using Game.Models;
+using Game;
+
+namespace Scenario
+{
+    [TestFixture]
+    public class BattlePageScenarioTests
+    {
+        App app;
+        BattlePage page;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new BattlePage();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
+        [Test]
+        public void BattlePage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
