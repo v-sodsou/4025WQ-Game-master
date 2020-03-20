588a7f8e3fecbf3dc97477d0cfe5e316e060ed80
Pedro De Almeida Voto <dealmeidavop@seattleu.edu>
2020-03-19 20:22:51 -0700

    Adding MonsterIndexViewModelTests.cs

---
+++ UnitTests/ViewModels/MonsterIndexViewModelTests.cs
@@ -0,0 +1,26 @@
+﻿using Game.ViewModels;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms.Mocks;
+
+namespace UnitTests.ViewModels
+{
+    class MonsterIndexViewModelTests
+    {
+        MonsterIndexViewModel ViewModel;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            // Add each model here to warm up and load it.
+            Game.Helpers.DataSetsHelper.WarmUp();
+
+            ViewModel = MonsterIndexViewModel.Instance;
+        }
+    }
+}
