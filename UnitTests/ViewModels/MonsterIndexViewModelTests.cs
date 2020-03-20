b7abbbb2e20e805f25abf51361014f4e61c02476
Pedro De Almeida Voto <dealmeidavop@seattleu.edu>
2020-03-19 20:36:27 -0700

    Adding Unit Test MonsterIndexViewModelTests.cs

--- UnitTests/ViewModels/MonsterIndexViewModelTests.cs
+++ UnitTests/ViewModels/MonsterIndexViewModelTests.cs
@@ -1,8 +1,10 @@
-﻿using Game.ViewModels;
+﻿using Game.Models;
+using Game.ViewModels;
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Xamarin.Forms.Mocks;
 
 namespace UnitTests.ViewModels
@@ -21,6 +23,64 @@ namespace UnitTests.ViewModels
             Game.Helpers.DataSetsHelper.WarmUp();
 
             ViewModel = MonsterIndexViewModel.Instance;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            MonsterIndexViewModel.Instance.Dataset.Clear();
+        }
+
+        [Test]
+        public async Task MonsterIndexViewModel_Read_Invalid_ID_Bogus_Should_Fail()
+        {
+            // Arrange
+
+            // Act
+            var result = await ViewModel.ReadAsync("bogus");
+
+            // Reset
+
+            // Assert
+            Assert.IsNull(result);
+        }
+
+        [Test]
+        public void MonsterIndexViewModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = ViewModel;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public void MonsterIndexViewModel_SortDataSet_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Add items into the list Z ordered
+            var dataList = new List<MonsterModel>
+            {
+                new MonsterModel { Name = "z" },
+                new MonsterModel { Name = "m" },
+                new MonsterModel { Name = "a" }
+            };
+
+            // Act
+            var result = ViewModel.SortDataset(dataList);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("a", result[0].Name);
+            Assert.AreEqual("m", result[1].Name);
+            Assert.AreEqual("z", result[2].Name);
         }
     }
 }
