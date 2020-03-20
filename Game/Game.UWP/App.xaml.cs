f3bc6b960125d7ae57a134c22c3077d2af85b629
Pedro De Almeida Voto <dealmeidavop@seattleu.edu>
2020-03-19 22:43:52 -0700

    Adding Unit Test AutoBattleEngineScenarioTest.cs

--- Game/Game.UWP/App.xaml.cs
+++ Game/Game.UWP/App.xaml.cs
@@ -54,11 +54,6 @@ namespace Game.UWP
 
                 Xamarin.Forms.Forms.Init(e);
 
-                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
-                {
-                    //TODO: Load state from previously suspended application
-                }
-
                 // Place the frame in the current Window
                 Window.Current.Content = rootFrame;
             }
@@ -94,7 +89,6 @@ namespace Game.UWP
         private void OnSuspending(object sender, SuspendingEventArgs e)
         {
             var deferral = e.SuspendingOperation.GetDeferral();
-            //TODO: Save application state and stop any background activity
             deferral.Complete();
         }
     }
